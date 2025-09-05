18a816b54f0a7b2e224140f3161bfd4aa5bc497f
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2025-09-05 09:23:01 +0300

    unicafe T1.7

--- Osa 1/Unicafe/src/App.jsx
+++ Osa 1/Unicafe/src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = good+bad+neutral
 
   return (
     <div>
@@ -24,6 +25,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {all}</p>
+      <p>Average {(all)/3}</p>
+      <p>Positive {good/all} %</p>
     </div>
   )
 }
