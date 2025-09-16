a4e0634fb618ebbb3d9105b1236b55b813fa01eb
Janne Siirtola <janne.siirtola@outlook.com>
2025-09-16 11:16:10 +0300

    puhelinluettelo T2.11 json-server

--- Osa 1/Puhelinluettelo/src/App.jsx
+++ Osa 1/Puhelinluettelo/src/App.jsx
@@ -1,29 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+
+import axios from 'axios'
 
 import PersonForm from './components/PersonForm'
 import Filter from './components/Filter'
 import NumbersDisplay from './components/NumbersDisplay'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  
+  const [persons, setPersons] = useState([])
+  const hook = () => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        const data = response.data
+        console.log('Data received:', data)
+        setPersons(data)
+      })
+  }
+
+  useEffect(hook, [])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setFilter] = useState('')
-
+  
   const addPerson = (event) => {
     event.preventDefault()
-
+    
     const exists = persons.some(person => person.name === newName)
     if (exists) {
       alert(`${newName} is already added to phonebook`)
       return
     }
-
+    
     setPersons(persons.concat({ name: newName, number: newNumber }))
     setNewName('')
     setNewNumber('')
@@ -43,8 +54,8 @@ const App = () => {
   const personsToShow = persons.filter(p =>
     p.name.toLowerCase().includes(filter.toLowerCase())
   )
-
-
+  
+  
   return (
     <div className="container">
       <h1>Phonebook</h1>
