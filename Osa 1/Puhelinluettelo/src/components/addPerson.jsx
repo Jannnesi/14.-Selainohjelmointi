ec9725f1ebd210d7a09952696ba0c7c7aa78d1e9
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2025-09-19 09:36:10 +0300

    2.15 Puhelinluettelo

--- Osa 1/Puhelinluettelo/src/components/addPerson.jsx
+++ Osa 1/Puhelinluettelo/src/components/addPerson.jsx
@@ -1,20 +1,53 @@
 import PersonService from "../services/PersonService"
 
-const addPerson = (event, persons, setPersons, newName, setNewName, newNumber, setNewNumber) => {
+const addPerson = (
+  event,
+  persons,
+  setPersons,
+  newName,
+  setNewName,
+  newNumber,
+  setNewNumber
+) => {
   event.preventDefault()
 
-  const exists = persons.some(person => person.name === newName)
-  if (exists) {
-    alert(`${newName} is already added to phonebook`)
-    return
+  const existingPerson = persons.find(p => p.name === newName)
+
+  if (existingPerson) {
+    // Update number if it’s different
+    if (newNumber !== existingPerson.number) {
+      const updatedPerson = { ...existingPerson, number: newNumber }
+
+      PersonService
+        .update(existingPerson.id, updatedPerson)
+        .then(response => {
+          setPersons(prev =>
+            prev.map(p => p.id !== existingPerson.id ? p : response.data)
+          )
+        })
+        .catch(err => {
+          alert(`Failed to update ${existingPerson.name}: ${err.message}`)
+        })
+    } else {
+      alert(`${newName} is already in the phonebook with the same number.`)
+    }
+  } else {
+    // Add new person
+    const newPerson = { name: newName, number: newNumber }
+
+    PersonService
+      .create(newPerson)
+      .then(response => {
+        setPersons(prev => prev.concat(response.data))
+      })
+      .catch(err => {
+        alert(`Failed to add ${newName}: ${err.message}`)
+      })
   }
 
-  const newPerson = { name: newName, number: newNumber }
-  setPersons(persons.concat(newPerson))
-  PersonService.create(newPerson)
+  // Reset inputs
   setNewName('')
   setNewNumber('')
-  console.log(`Added ${newName}`)
 }
 
 export default addPerson
