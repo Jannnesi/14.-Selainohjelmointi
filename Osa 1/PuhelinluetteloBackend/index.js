95cff4032cbb4e11cd51afa2d8b810996088415e
jannesi-projects <116154575+jannesi-projects@users.noreply.github.com>
2025-09-23 12:35:12 +0300

    T3.1-3.6 Puhelinluettelo backend

---
+++ Osa 1/PuhelinluetteloBackend/index.js
@@ -0,0 +1,71 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+persons = [
+    {
+      "name": "Arto Hellas",
+      "number": "040-123456",
+      "id": "1"
+    },
+    {
+      "name": "Ada Lovelace",
+      "number": "39-44-5323523",
+      "id": "2"
+    },
+    {
+      "name": "Dan Abramov",
+      "number": "12-43-234345",
+      "id": "3"
+    },
+    {
+      "name": "Mary Poppendieck",
+      "number": "39-23-6423122",
+      "id": "4"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+})
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<p></p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    if (!person) return response.status(404).end()
+    response.json(person)
+})
+
+app.post('/api/persons', (request, response) => {
+    const person = request.body
+    if (!person.name || !person.number) {
+        return response.status(400).json({ error: 'Name and number are required' })
+    }
+    if (persons.find(p => p.name === person.name)) {
+        return response.status(400).json({ error: 'Name must be unique' })
+    }
+    person.id = (Math.random() * 1000000).toFixed(0)
+    persons.push(person)
+    response.status(201).json(person)
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
